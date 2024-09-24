7e2abe56011fb46be2063d86ba28bc3738d9d969
feeelibread <feeelibread@outlook.com>
2024-09-24 18:22:42 -0300

    Feito o desafio de POO da trilha dotnet

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Feito: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "11912345678", modelo: "Tijolo", imei: "1234567890", memoria: 256);
+Smartphone iphone = new Iphone(numero: "11998765432", modelo: "a4", imei: "098765432", memoria: 2);
+
+nokia.InstalarAplicativo("Spotifly");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+
+iphone.InstalarAplicativo("Safaris");
+iphone.Ligar();
+iphone.ReceberLigacao();
